6fb1921144c12d1e47414c648da247598fa4c520
Mateus Eduardo <mateussilva184@gmail.com>
2023-05-10 15:54:09 -0300

    Arquivo script.js criado

---
+++ scripts.js
@@ -0,0 +1,11 @@
+console.log('oi')
+
+console.log ('fumegando')
+
+function Calculadora(event) {
+
+ }
+
+document.getElementById('totalTips').style.display = "none"
+document.getElementById('each').style.display = "none"
+
